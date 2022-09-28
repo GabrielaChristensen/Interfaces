c1705a583389b9b5dc046fc1bcb6c0f37546d6b9
gabriela christensen <gchristensen@alumnos.exa.unicen.edu.ar>
2022-09-28 17:36:45 -0300

    menu, images, etc

--- Entregable 2/js/main.js
+++ Entregable 2/js/main.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let desplegable = document.querySelector(".menu-navbar");
     let btnMenu = document.querySelector(".menu-hamburguesa");
     btnMenu.addEventListener("click", () => {
+        console.log('test', desplegable);
         desplegable.classList.toggle("menu-navbar-visible");
     })
 })
